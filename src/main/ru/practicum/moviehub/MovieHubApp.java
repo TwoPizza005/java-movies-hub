87b9639182dfd20188932bdeef0453847da14224
TwoPizza <radmir.yakupov94@mail.ru>
2026-07-30 19:40:58 +0300

    Initial commit: MovieHub project

--- src/main/ru/practicum/moviehub/MovieHubApp.java
+++ src/main/ru/practicum/moviehub/MovieHubApp.java
@@ -3,10 +3,14 @@ package ru.practicum.moviehub;
 import ru.practicum.moviehub.http.MoviesServer;
 import ru.practicum.moviehub.store.MoviesStore;
 
+import java.io.IOException;
+
 public class MovieHubApp {
-    public static void main(String[] args) {
-        final MoviesServer server = new MoviesServer(new MoviesStore(), 8080);
+    public static void main(String[] args) throws IOException {
+        MoviesStore store = new MoviesStore();
+        final MoviesServer server = new MoviesServer(store, 8080);
         Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
         server.start();
+        System.out.println("Сервер запущен на http://localhost:8080/movies");
     }
 }
--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -1,5 +1,50 @@
 package ru.practicum.moviehub.model;
 
 public class Movie {
+    private String nameMovie;
+    private int year;
+    private long id;
 
+    // Конструктор без ID (для создания нового фильма)
+    public Movie(int year, String nameMovie) {
+        this.year = year;
+        this.nameMovie = nameMovie;
+    }
+
+    public Movie() {
+    }
+
+    // Геттеры и сеттеры
+    public String getNameMovie() {
+        return nameMovie;
+    }
+
+    public void setNameMovie(String nameMovie) {
+        this.nameMovie = nameMovie;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "nameMovie='" + nameMovie + '\'' +
+                ", year=" + year +
+                ", id=" + id +
+                '}';
+    }
 }
